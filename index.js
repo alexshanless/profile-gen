b493e4383af180ba27f1da75ffe808ae87685dc6
Alex Bilba <alex.shanless@gmail.com>
2020-02-12 18:09:07 -0800

    uploading base

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const convertFactory = require('electron-html-to');
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() { }
+
+init();
